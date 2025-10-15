fa3e294c52bf38a4f2f60627646d3a1e8996fb69
Joseph Emmanuel NKOULOU <joseph.nkoulou@2027.ucac-icam.com>
2025-10-15 05:06:56 +0200

    Page Meteo Okay

--- src/components/components_weather/HourlyWeather.jsx
+++ src/components/components_weather/HourlyWeather.jsx
@@ -1,16 +1,27 @@
-import { weatherCodes } from "../constants_weather";
+import styles from '../weather.module.css';
+import { weatherCodes } from '../constants_weather';
+
 const HourlyWeatherItem = ({ hourlyWeather }) => {
   // Extract and format temperature and time
   const temperature = Math.floor(hourlyWeather.temp_c);
-  let time = hourlyWeather.time.split(" ")[1].substring(0, 5);
+  const time = hourlyWeather.time.split(' ')[1].substring(0, 5);
+
   // Find the appropriate weather icon
-  const weatherIcon = Object.keys(weatherCodes).find((icon) => weatherCodes[icon].includes(hourlyWeather.condition.code));
+  const weatherIcon = Object.keys(weatherCodes).find((icon) =>
+    weatherCodes[icon].includes(hourlyWeather.condition.code)
+  );
+
   return (
-    <li className="weather-item">
-      <p className="time">{time}</p>
-      <img src={`icons_weather/${weatherIcon}.svg`} className="weather-icon" />
-      <p className="temperature">{temperature}°</p>
+    <li className={styles['weather-item']}>
+      <p className={styles.time}>{time}</p>
+      <img
+        src={`icons_weather/${weatherIcon}.svg`}
+        className={styles['weather-icon']}
+        alt="weather icon"
+      />
+      <p className={styles.temperature}>{temperature}°</p>
     </li>
   );
 };
+
 export default HourlyWeatherItem;
--- src/components/components_weather/NoResultsDiv.jsx
+++ src/components/components_weather/NoResultsDiv.jsx
@@ -1,10 +1,19 @@
+import styles from '../weather.module.css';
+
 const NoResultsDiv = () => {
   return (
-    <div className="no-results">
-      <img src="icons_weather/no-result.svg" alt="No results found" className="icon" />
-      <h3 className="title">Something went wrong!</h3>
-      <p className="message">We&apos;re unable to retrieve the weather details. Enure you&apos;ve entered a valid city or try again later.</p>
+    <div className={styles['no-results']}>
+      <img
+        src="icons_weather/no-result.svg"
+        alt="No results found"
+        className={styles.icon}
+      />
+      <h3 className={styles.title}>Something went wrong!</h3>
+      <p className={styles.message}>
+        We&apos;re unable to retrieve the weather details. Ensure you&apos;ve entered a valid city or try again later.
+      </p>
     </div>
   );
 };
+
 export default NoResultsDiv;
